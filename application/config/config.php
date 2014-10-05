02cc204e0e8c727cf487802eb06e1d7c0644fe4f
grvaz <shirokov.val@gmail.com>
2014-10-05 18:21:51 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://dialog-seversk.ru/';
+$config['base_url']	= 'http://domain.ru/';
 
 /*
 |--------------------------------------------------------------------------
